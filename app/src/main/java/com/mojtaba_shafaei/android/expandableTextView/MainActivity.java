07ebc1237fe5c4397ab2923ec5be22e7aee8eb5a
mojtaba <shafaei.saeid@gmail.com>
2018-12-01 14:47:58 +0330

    Improve ui upgrade support library version

--- app/src/main/java/com/mojtaba_shafaei/android/expandableTextView/MainActivity.java
+++ app/src/main/java/com/mojtaba_shafaei/android/expandableTextView/MainActivity.java
@@ -19,23 +19,18 @@ public class MainActivity extends AppCompatActivity {
     et.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 14);
     //et.setImageResource(android.R.drawable.ic_delete);
 
-    et.setText(
+    /*et.setText(
         "Vivamus suscipit tortor eget felis porttitor volutpat. Mauris\n blandit aliquet elit,\n eget tincidunt nibh pulvinar a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Cras ultricies ligula sed magna dictum porta. Curabitur aliquet quam id dui posuere blandit. Nulla quis lorem ut libero malesuada feugiat. Donec sollicitudin molestie malesuada. Proin eget tortor risus. Mauris blandit aliquet elit,\n eget tincidunt nibh pulvinar a.");
 
-    //et.expand();
+    */
+    et.setText("لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.\n"
+                   + "\n"
+                   + "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.");
 
-    /*et.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        et.setStatus(Status.COLLAPSED);
-        //Log.d("ssss", "onClick: ");
-      }
-    });*/
   }
 
   @Override
   protected void onDestroy() {
     super.onDestroy();
-    et.dispose();
   }
 }
--- library/src/main/java/com/mojtaba_shafaei/android/ExpandableTextView.java
+++ library/src/main/java/com/mojtaba_shafaei/android/ExpandableTextView.java
@@ -1,254 +1,194 @@
 package com.mojtaba_shafaei.android;
 
-import static com.mojtaba_shafaei.android.Status.COLLAPSED;
-import static com.mojtaba_shafaei.android.Status.EXPANDED;
+import static com.mojtaba_shafaei.android.ExpandableTextView.Status.COLLAPSED;
+import static com.mojtaba_shafaei.android.ExpandableTextView.Status.EXPANDED;
 
-import android.animation.ObjectAnimator;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
-import android.animation.ValueAnimator.AnimatorUpdateListener;
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.content.res.ColorStateList;
-import android.graphics.Typeface;
+import android.graphics.Canvas;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.mojtaba_shafaei.com.library.R;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.annotation.ColorInt;
-import android.support.annotation.DrawableRes;
 import android.support.annotation.IntRange;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.AppCompatTextView;
+import android.text.TextUtils.TruncateAt;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
-public class ExpandableTextView extends RelativeLayout {
+public class ExpandableTextView extends AppCompatTextView{
 
-  private final String TAG = getClass().getSimpleName();
-  private TextView tvDescription;
-  private AppCompatImageView btnExpand;
-  private int status = COLLAPSED;
+private final String TAG = "ExpandableTextView";
+private Status status = COLLAPSED;
 
-  private int MIN_ROW = 2;
+private int MIN_ROW = 2;
 
-  private ValueAnimator expandingRotateAnimator;
+//////////////////////////////////////
+private Drawable _drawable;
 
-  //////////////////////////////////////
-  private final java.lang.String STATUS_KEY = "EXPANDED";
+//////////////////////////////////////
 
-  //////////////////////////////////////
-  private OnClickListener onClickListener = new OnClickListener() {
+public ExpandableTextView(Context context){
+  super(context);
+  init(context);
+}
+
+public ExpandableTextView(Context context, AttributeSet attrs){
+  super(context, attrs);
+  init(context);
+}
+
+public ExpandableTextView(Context context,
+    @Nullable AttributeSet attrs, int defStyleAttr){
+  super(context, attrs, defStyleAttr);
+  init(context);
+}
+
+private void init(Context context){
+  _drawable = ContextCompat.getDrawable(context, R.drawable.etv_ic_arrow_drop_down_24dp);
+
+  setDrawable(_drawable);
+  setCompoundDrawablePadding(getResources().getDimensionPixelSize(R.dimen.etv_drawable_padding));
+
+  setOnClickListener(new OnClickListener(){
     @Override
-    public void onClick(View view) {
-      if (status == COLLAPSED) {
-        setStatus(EXPANDED);
-      } else {
-        setStatus(COLLAPSED);
+    public void onClick(View v){
+      toggle();
+    }
+  });
+
+}
+
+public void setText(CharSequence text, boolean showDrawable){
+  super.setText(text);
+  if(text.length() > 0 && showDrawable){
+    post(new Runnable(){
+      @Override
+      public void run(){
+        if(getLineCount() <= 1){
+          setDrawable(null);
+        } else{
+          setDrawable(getRotateDrawable(_drawable, 0));
+        }
       }
+    });
+  } else{
+    setDrawable(null);
+  }
+
+  refreshDrawableState();
+}
+
+public void setDrawable(Drawable drawable){
+  setCompoundDrawablesWithIntrinsicBounds(null, null, null, drawable);
+}
+
+public void setDrawableTintColor(@ColorInt int color){
+  _drawable.setColorFilter(color, Mode.SRC_ATOP);
+  refreshDrawableState();
+}
+
+private void collapseTextView(){
+  try{
+    clearAnimation();
+
+    final ValueAnimator animator = ValueAnimator.ofInt(getLineCount(), MIN_ROW).setDuration(250);
+    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+      int lastValue = -1;
+
+      @Override
+      public void onAnimationUpdate(ValueAnimator animation){
+        int value = (int) animation.getAnimatedValue();
+        if(value == lastValue){
+          return;
+        }
+        lastValue = value;
+
+        setMaxLines(value);
+      }
+    });
+    animator.addListener(new AnimatorListenerAdapter(){
+      @Override
+      public void onAnimationEnd(Animator animation){
+        super.onAnimationEnd(animation);
+        setDrawable(getRotateDrawable(_drawable, 0));
+        animator.removeAllListeners();
+        animator.removeAllUpdateListeners();
+      }
+    });
+    animator.start();
+
+    setEllipsize(TruncateAt.END);
+    status = COLLAPSED;
+  } catch(Exception e){
+    Log.e("Utility", "collapseTextView: " + e);
+  }
+
+}
+
+private void expandTextView(){
+  try{
+    clearAnimation();
+    final ValueAnimator animator = ValueAnimator.ofInt(MIN_ROW, 100).setDuration(400);
+    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+
+      @Override
+      public void onAnimationUpdate(ValueAnimator animation){
+        int value = (int) animation.getAnimatedValue();
+        setMaxLines(value);
+      }
+    });
+    animator.addListener(new AnimatorListenerAdapter(){
+      @Override
+      public void onAnimationEnd(Animator animation){
+        super.onAnimationEnd(animation);
+        setDrawable(getRotateDrawable(_drawable, 180));
+        animator.removeAllListeners();
+        animator.removeAllUpdateListeners();
+      }
+    });
+    animator.start();
+    setEllipsize(null);
+    status = EXPANDED;
+
+  } catch(Exception e){
+    Log.e("Utility", "ExpandTextView: " + e);
+  }
+}
+
+public void setMinRows(@IntRange(from = 1, to = 50) int minRows){
+  this.MIN_ROW = minRows;
+}
+
+public void toggle(){
+  if(status == COLLAPSED){
+    expandTextView();
+  } else{
+    collapseTextView();
+  }
+}
+
+public enum Status{
+
+  EXPANDED, COLLAPSED
+}
+
+private Drawable getRotateDrawable(final Drawable d, final float angle){
+  final Drawable[] arD = {d};
+  return new LayerDrawable(arD){
+    @Override
+    public void draw(final Canvas canvas){
+      canvas.save();
+      canvas.rotate(angle, d.getBounds().width() / 2, d.getBounds().height() / 2);
+      super.draw(canvas);
+      canvas.restore();
     }
   };
-
-  public ExpandableTextView(Context context) {
-    super(context);
-    init(context);
-  }
-
-  public ExpandableTextView(Context context, AttributeSet attrs) {
-    super(context, attrs);
-    init(context);
-  }
-
-  public ExpandableTextView(Context context,
-      @Nullable AttributeSet attrs, int defStyleAttr) {
-    super(context, attrs, defStyleAttr);
-    init(context);
-  }
-
-  @TargetApi(21)
-  public ExpandableTextView(Context context, AttributeSet attrs, int defStyleAttr,
-      int defStyleRes) {
-    super(context, attrs, defStyleAttr, defStyleRes);
-    init(context);
-  }
-
-  private void init(Context context) {
-    inflate(context, R.layout.layout, this);
-    tvDescription = findViewById(R.id.tv_description);
-    btnExpand = findViewById(R.id.btnExpand);
-
-    btnExpand.setImageDrawable(
-        ContextCompat.getDrawable(context, R.drawable.ic_arrow_drop_down_24dp)
-    );
-
-    tvDescription.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        if (onClickListener != null) {
-          onClickListener.onClick(view);
-        }
-      }
-    });
-
-    btnExpand.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        if (onClickListener != null) {
-          onClickListener.onClick(view);
-        }
-      }
-    });
-
-    expandingRotateAnimator = ValueAnimator.ofFloat(0, 180);
-    expandingRotateAnimator.addUpdateListener(new AnimatorUpdateListener() {
-      @Override
-      public void onAnimationUpdate(ValueAnimator valueAnimator) {
-        btnExpand.setRotation((Float) valueAnimator.getAnimatedValue());
-      }
-    });
-
-    btnExpand.setRotation(0);
-  }
-
-  public void setText(String text) {
-    tvDescription.setText(text);
-    if (text != null && !text.isEmpty()) {
-      tvDescription.post(new Runnable() {
-        @Override
-        public void run() {
-          if (tvDescription.getLineCount() <= 1) {
-            btnExpand.setVisibility(INVISIBLE);
-          } else {
-            btnExpand.setVisibility(VISIBLE);
-          }
-        }
-      });
-    }
-
-    refreshDrawableState();
-  }
-
-
-  private void collapseTextView(final int numLines) {
-    try {
-      tvDescription.post(new Runnable() {
-        @Override
-        public void run() {
-          if (Build.VERSION.SDK_INT > 16) {
-            ObjectAnimator animation = ObjectAnimator.ofInt(tvDescription, "maxLines", numLines);
-            animation.setDuration(300).start();
-            tvDescription.setMaxLines(numLines);
-          } else {
-            tvDescription.setMaxLines(numLines);
-          }
-        }
-      });
-
-    } catch (Exception e) {
-      Log.e("Utility", "collapseTextView: " + e);
-    }
-  }
-
-  private void expandTextView() {
-    try {
-      tvDescription.post(new Runnable() {
-        @Override
-        public void run() {
-          if (Build.VERSION.SDK_INT > 16) {
-            int lc = tvDescription.getLineCount();
-            ObjectAnimator animation = ObjectAnimator.ofInt(tvDescription, "maxLines", lc);
-            animation.setDuration(Math.max(lc * 10, 300)).start();
-          } else {
-            tvDescription.setMaxLines(tvDescription.getLineCount());
-          }
-        }
-      });
-    } catch (Exception e) {
-      Log.e("Utility", "expandTextView: " + e);
-    }
-
-  }
-
-  public void setMinRows(@IntRange(from = 0, to = 100) int minRows) {
-    this.MIN_ROW = minRows;
-  }
-
-
-  public void setTypeface(Typeface typeface) {
-    tvDescription.setTypeface(typeface);
-  }
-
-  public void setTextSize(float textSize) {
-    tvDescription.setTextSize(textSize);
-  }
-
-  public void setTextSize(int unit, float textSize) {
-    tvDescription.setTextSize(unit, textSize);
-  }
-
-  public void setTextColor(@ColorInt int color) {
-    tvDescription.setTextColor(color);
-  }
-
-  public void setTextColor(ColorStateList color) {
-    tvDescription.setTextColor(color);
-  }
-
-
-  public void setStatus(@Status int status) {
-    this.status = status;
-    if (status == COLLAPSED) {
-      collapseTextView(MIN_ROW);
-      expandingRotateAnimator.reverse();
-    } else {
-      expandTextView();
-      expandingRotateAnimator.start();
-    }
-  }
-
-  public void setImageResource(@DrawableRes int resId) {
-    btnExpand.setImageResource(resId);
-  }
-
-  @Override
-  public void setEnabled(boolean enabled) {
-    super.setEnabled(enabled);
-//    btnExpand.setEnabled(enabled);
-//    tvDescription.setEnabled(enabled);
-  }
-
-  @Override
-  public void setOnClickListener(@Nullable OnClickListener l) {
-    this.onClickListener = l;
-  }
-
-  @Nullable
-  @Override
-  protected Parcelable onSaveInstanceState() {
-    Bundle bundle = new Bundle();
-    bundle.putParcelable("superState", super.onSaveInstanceState());
-    bundle.putInt(STATUS_KEY, this.status);
-    return bundle;
-  }
-
-  @Override
-  protected void onRestoreInstanceState(Parcelable state) {
-    if (state instanceof Bundle) {
-      Bundle bundle = (Bundle) state;
-      setStatus(bundle.getInt(STATUS_KEY));
-
-      state = bundle.getParcelable("superState");
-    }
-
-    super.onRestoreInstanceState(state);
-  }
-
-  public void dispose() {
-    expandingRotateAnimator.removeAllUpdateListeners();
-    expandingRotateAnimator.removeAllListeners();
-    expandingRotateAnimator = null;
-  }
+}
 }
